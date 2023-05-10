70770f8009ec4214318d465455541f2155874617
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2023-05-10 18:55:18 +0200

    jwt in a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require("../middleware/async.js");
 const User = require("../models/User.js");
 
 //@desc     Register a user
-//@route    GET /api/v1/auth/register
+//@route    POST /api/v1/auth/register
 //@access   Public
 
 exports.register = asyncHandler(async (req, res, next) => {
@@ -16,5 +16,63 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  res.status(200).json({ success: true });
+  //create token
+
+  sendTokenResponse(user, 200, res);
 });
+
+//@desc     Login a user
+//@route    POST /api/v1/auth/login
+//@access   Public
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //validate email and pw
+  if (!email || !password) {
+    return next(
+      new errorResponse("Please provide an email and a password", 400)
+    );
+  }
+
+  //check for user
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new errorResponse("Credentials invalid", 401));
+  }
+
+  //check if password matches
+
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new errorResponse("Credentials invalid", 401));
+  }
+
+  //create token
+
+  sendTokenResponse(user, 200, res);
+});
+
+// get token from model
+
+const sendTokenResponse = (user, statusCode, res) => {
+  const token = user.getSignedJwtToken();
+
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NOVE_ENV === "production") {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({ success: true, token: token });
+};
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -40,5 +41,18 @@ UserSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+//sign JWT and return
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+// Match pw to hashed pw
+
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", UserSchema);
